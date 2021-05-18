9627a2a84439941d24503eb9d9abd1d7ec1cc2d6
chrlen <christian.lengert@tuta.io>
2021-05-18 13:40:53 +0200

    Add default.nix

---
+++ default.nix
@@ -0,0 +1,16 @@
+with (import <nixpkgs> { });
+let
+mach-nix = import
+  (builtins.fetchGit {
+    url = "https://github.com/DavHau/mach-nix/";
+    ref = "refs/tags/3.2.0";
+  })
+  {
+    python = "python38";
+  };
+in
+mkShell {
+  buildInputs = [
+    protobuf
+  ];
+}
